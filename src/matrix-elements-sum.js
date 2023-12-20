8b5e80ac84dde5683c507db5958b2b731a82c8c7
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2023-12-20 22:26:24 +0300

    feat: make task matrix-elements-sum.js, attempt to make task what-season.js

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  let sum = 0;
+  const digitsBelowZeros = new Array(cols).fill(false);
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (!digitsBelowZeros[j]) {
+        sum += matrix[i][j];
+      }
+      if (matrix[i][j] === 0) {
+        digitsBelowZeros[j] = true;
+      }
+    }
+  }
+
+  return sum;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -12,6 +12,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(/* date */) {
+  // if (date === undefined || date === null) {
+  //   return 'Unable to determine the time of year!';
+  // }
+
+  // if (!(date instanceof Date) || isNaN(date.getTime())) {
+  //   throw new Error('Invalid date!');
+  // }
+
+  // const month = date.getMonth();
+  // if (month >= 2 && month <=4) {
+  //   return 'spring';
+  // } else if (month >= 5 && month <= 7) {
+  //   return 'summer';
+  // } else if ( month >= 8 && month <= 10) {
+  //   return 'autumn';
+  // } else {
+  //   return 'winter';
+  // }
+
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
